fb50123a889f455ecb3fd23f23f08a3189d7a878
이경호 <no_rudgh@naver.com>
2026-09-04 17:42:36 +0900

    행렬 operator 구현
    행렬 operator 구현

---
+++ Week2/Matrix.h
@@ -0,0 +1,114 @@
+﻿#pragma once
+
+struct FMatrix { 
+	float M[4][4];
+
+	FMatrix Inverse(FMatrix) const; // 4 x 4 역행렬
+		
+	
+	static const FMatrix Identity;
+
+	static FMatrix makeIdentity() // 단위행렬 만드는 함수
+	{
+		FMatrix R = {};
+		R.M[0][0] = R.M[1][1] = R.M[2][2] = R.M[3][3] = 1.0f;
+		return R;
+	}
+
+	FMatrix operator* (const FMatrix& Other) const  // 4 x 4 행렬곱
+	{
+		FMatrix result = {};
+
+		for (int row = 0; row < 4;++row) {
+			for (int col = 0;col < 4;++col) {
+				for (int k = 0;k < 4;++k) {
+					result.M[row][col] += M[row][k] * Other.M[k][col];
+				}
+			}
+		}
+		return result;
+	}
+
+	FMatrix operator*(float Scalar) const  // M x 상수(Sclalar) ,Scaling 아님
+	{ 
+		FMatrix result;
+		for (int row = 0; row < 4; ++row) {
+			for (int col = 0; col < 4; ++col) {
+				result.M[row][col] = M[row][col] * Scalar;
+			}
+		}
+
+		return result;
+	}
+
+
+	FMatrix operator+ (const FMatrix& Other) const   // M + M , 행렬끼리의 합
+	{ 
+		FMatrix result = {};
+
+		for (int row = 0; row < 4;++row) {
+			for (int col = 0;col < 4;++col) {
+				result.M[row][col] = M[row][col] + Other.M[row][col];
+			}
+		}
+		return result;
+	}
+
+
+
+	FMatrix operator- (const FMatrix& Other) const  // M - M , 행렬끼리의 차
+	{ 
+		FMatrix result = {};
+
+		for (int row = 0; row < 4;++row) {
+			for (int col = 0;col < 4;++col) {
+				result.M[row][col] = M[row][col] - Other.M[row][col];
+			}
+		}
+		return result;
+	}
+
+
+	FMatrix operator+(float f) const // M + f : 각원소 덧셈(f)
+	{ 
+		FMatrix result = {};
+		for (int row = 0; row < 4; ++row) {
+			for (int col = 0; col < 4; ++col) {
+				result.M[row][col] = M[row][col] + f;
+			}
+		}
+		return result;
+	}
+
+	FMatrix operator-(float f) const // M - f : 각원소 뺄셈(f)
+	{ 
+		FMatrix result={};
+		for (int row = 0; row < 4; ++row) {
+			for (int col = 0; col < 4; ++col) {
+				result.M[row][col] = M[row][col] - f;
+			}
+		}
+		return result;
+	}
+
+
+
+	FMatrix Transpose() const // 전치행렬
+	{ 
+		FMatrix result = {};
+		for (int row = 0; row < 4; ++row) {
+			for (int col = 0; col < 4; ++col) {
+				result.M[row][col] = M[col][row];
+			}
+		}
+		return result;
+	};
+
+};
+
+inline const FMatrix FMatrix::Identity = { {
+	{1, 0, 0, 0},
+	{0, 1, 0, 0},
+	{0, 0, 1, 0},
+	{0, 0, 0, 1}
+} };
