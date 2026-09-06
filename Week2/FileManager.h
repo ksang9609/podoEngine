3ec3e526d01842b9ed3b1f99cdc85d888dd72b57
Sanghyeon Kim <ksangh9609@gmail.com>
2026-09-06 15:10:52 +0900

    Add File Manager

---
+++ Week2/FileManager.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include <filesystem>
+
+#include "Core.h"
+
+inline constexpr std::string_view kDefaultRootPath = ".\\";
+inline constexpr std::string_view kDefaultAssetsPath = ".\\Assets\\";
+inline constexpr std::string_view kSceneDataSuffix = ".Scene";
+
+class FFileManager
+{
+public:
+	FFileManager();
+	FFileManager(std::string_view fileDirPath);
+	FFileManager(std::string_view fileDirPath, std::string_view rootPath);
+
+	FString ReadFileToString(std::string_view fileName);
+	void WriteStringToFile(std::string_view fileName, std::string_view content);
+
+private:
+	std::filesystem::path mFileDirPath;
+	std::filesystem::path mRootPath;
+
+	bool IsUnderRoot(const std::filesystem::path& filePath) const;
+	bool IsUnderFileDir(const std::filesystem::path& filePath) const;
+};
+
+bool IsUnder(const std::filesystem::path& filePath, const std::filesystem::path& rootPath);
