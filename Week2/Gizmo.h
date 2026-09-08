db8004710de41f7b1f2a6ebda04c3f51c1e4d288
이경호 <no_rudgh@naver.com>
2026-09-08 13:36:10 +0900

    기즈모 rotate 하이라이트 구현

--- Week2/Gizmo.h
+++ Week2/Gizmo.h
@@ -15,6 +15,7 @@ struct FGizmo {
 	enum EGIZMO_TYPE {
 		TRANSLATE,
 		ROTATE,
+		SCALE,
 	};
 
 
@@ -26,12 +27,12 @@ struct FGizmo {
 	float mGizmoScale=1.0f;
 	float mAxisLength = mGizmoScale * 1.0f;
 	float mAxisThickness = mGizmoScale * 0.2f;
-	float mHitRadius= mAxisThickness*1.1f; // 마우스 판정보정
-	//float mRingHitRadius = mAxisThickness * 1.1f; // 마우스 판정보정
+	float mHitRadius= mAxisThickness*1.1f; // Translate 마우스 판정보정
+	float mRingHitRadius = 0.3f; // Rotate마우스 판정보정 (+0.08배)
 	float mGizmoSizeRatio = 0.2f;
 	EGIZMO_AXIS eAxis = NONE; // 축위에 있는지
 	EGIZMO_AXIS mDraggingAxis = NONE; // Drag중인 축
-	EGIZMO_TYPE eType=TRANSLATE;
+	EGIZMO_TYPE eType=ROTATE;
 
 	FVector AxisDirection(EGIZMO_AXIS axis) const {
 		switch (axis)
@@ -69,73 +70,73 @@ struct FGizmo {
 
 		if (eType == ROTATE) //회전 기즈모의 충돌처리
 		{
-
+			float shortAxisLen = 0.0f;
 			for (int i = 0; i < 3; ++i)
-		{
+			{
+
 				//t x norm_ray = 링위의점
 				float t = FVector::dot((mLocation - nearPoint), AxisDirection(axis[i]))
 					/ FVector::dot(norm_ray, AxisDirection(axis[i]));
 
+				if (t < 0.0f) continue;
+
 				FVector H = (norm_ray * t) + nearPoint;
 				float r = (H-mLocation).Length(); //구 중심과 평면교점사이의 거리
 
-				if (FMath::Abs(r - mGizmoScale) < 0.1e-4f) continue;
-				if (t < 0.0f) continue;
-				if (t < 0.0f) continue;
-		}
+				if (FMath::Abs(r - mGizmoScale) > mGizmoScale *mRingHitRadius) continue;
+
+				if (eAxis == NONE || t < shortAxisLen) {
+					shortAxisLen = t;
+					eAxis = axis[i];
+				}
+			}
 
 
 		}
 
 
 
+		
 
+		else if (eType == TRANSLATE) {
+			FVector w0 = nearPoint - mLocation; // \
+					 //수학 함수 구현
+			const float axisLength = mAxisLength * mGizmoScale;
+			const float hitRadius = mHitRadius * mGizmoScale;
 
+			float bestRayT = 0.0f; //near point에서 광선방향으로 얼마나 이동했냐
 
-
-
-
-
-
-
-		FVector w0 = nearPoint - mLocation; // \
-		 //수학 함수 구현
-		const float axisLength = mAxisLength * mGizmoScale;
-		const float hitRadius = mHitRadius * mGizmoScale;
-
-		float bestRayT = 0.0f; //near point에서 광선방향으로 얼마나 이동했냐
-
-		for (int i = 0; i < 3; ++i)
-		{
-			const FVector axisDir = AxisDirection(axis[i]);
-
-			const float align = FVector::dot(norm_ray, axisDir);
-			const float rayProj = FVector::dot(norm_ray, w0);
-			const float axisProj = FVector::dot(axisDir, w0);
-
-			const float denom = 1.0f - align * align;
-			if (FMath::Abs(denom) < 1e-5f) continue;        // 레이와 축이 거의 나란함
-
-			float axisS = (axisProj - align * rayProj) / denom;
-			axisS = FMath::Clamp(axisS, 0.0f, axisLength);  // 무한 직선 → 선분
-
-			const FVector axisPoint = mLocation + axisDir * axisS; // 현재위치에서 기즈모방향으로 얼만큼 이동했나
-
-			const float rayT = FVector::dot(norm_ray, axisPoint - nearPoint);
-			if (rayT < 0.0f) continue;                      // 카메라 뒤쪽
-
-			const FVector rayPoint = nearPoint + norm_ray * rayT;
-			const float distance = (rayPoint - axisPoint).Length();
-
-			if (distance > hitRadius) continue;             // 캡슐 밖
-
-			if (eAxis == NONE || rayT < bestRayT)           // 겹치면 카메라에 가까운 축
+			for (int i = 0; i < 3; ++i)
 			{
-				bestRayT = rayT;
-				eAxis = axis[i];
+				const FVector axisDir = AxisDirection(axis[i]);
+
+				const float align = FVector::dot(norm_ray, axisDir);
+				const float rayProj = FVector::dot(norm_ray, w0);
+				const float axisProj = FVector::dot(axisDir, w0);
+
+				const float denom = 1.0f - align * align;
+				if (FMath::Abs(denom) < 1e-5f) continue;        // 레이와 축이 거의 나란함
+
+				float axisS = (axisProj - align * rayProj) / denom;
+				axisS = FMath::Clamp(axisS, 0.0f, axisLength);  // 무한 직선 → 선분
+
+				const FVector axisPoint = mLocation + axisDir * axisS; // 현재위치에서 기즈모방향으로 얼만큼 이동했나
+
+				const float rayT = FVector::dot(norm_ray, axisPoint - nearPoint);
+				if (rayT < 0.0f) continue;                      // 카메라 뒤쪽
+
+				const FVector rayPoint = nearPoint + norm_ray * rayT;
+				const float distance = (rayPoint - axisPoint).Length();
+
+				if (distance > hitRadius) continue;             // 캡슐 밖
+
+				if (eAxis == NONE || rayT < bestRayT)           // 겹치면 카메라에 가까운 축
+				{
+					bestRayT = rayT;
+					eAxis = axis[i];
+				}
 			}
 		}
-
 		return eAxis != NONE;
 	}
 
@@ -151,7 +152,7 @@ struct FGizmo {
 		{
 		case TRANSLATE: return EPrimitive::EP_GizmoArrow;
 		case ROTATE: return EPrimitive::EP_CirCle; //EP_Rotate
-		default: break;
+		default: return EPrimitive::EP_GizmoArrow;
 		}
 	}
 
@@ -163,11 +164,14 @@ struct FGizmo {
 		const FRotator rotation = FRotator::FromDirection(AxisDirection(axis));
 		if (eType == ROTATE)
 		{
-			return FMatrix::Scale(FVector( mGizmoScale))
-				* FMatrix::Rotate(rotation)
-				* FMatrix::Translation(mLocation);
+			const EGIZMO_AXIS axis[3] = { X, Y, Z };
+			for (int i = 0;i < 3;i++) {
+				return FMatrix::Scale(FVector(mGizmoScale))
+					* FMatrix::Rotate(rotation)
+					* FMatrix::Translation(mLocation);
+			}
 		}
-		else { //eType= Translate
+		else if(eType == ROTATE) { //eType= Translate
 			return FMatrix::Scale(FVector(length, thickness, thickness))
 				* FMatrix::Translation(FVector(0.0f, -thickness * 0.5f, -thickness * 0.5f)) // 긴막대기 모양으로변환
 				* FMatrix::Rotate(rotation)
@@ -177,14 +181,15 @@ struct FGizmo {
 
 	FVector4 GetAxisColor(EGIZMO_AXIS axis) const
 	{
-		if (axis == eAxis) return FVector4(1.0f, 1.0f, 0.0f, 1.0f);   // 마우스가 올라간 축
+		float alpha =1.0f;
+		if (axis == eAxis) return FVector4(1.0f, 1.0f, 1.0f, 1.0f);   // 마우스가 올라간 축
 
 		switch (axis)
 		{
-		case X:  return FVector4(1.0f, 0.0f, 0.0f, 1.0f);
-		case Y:  return FVector4(0.0f, 1.0f, 0.0f, 1.0f);
-		case Z:  return FVector4(0.0f, 0.4f, 1.0f, 1.0f);
-		default: return FVector4(0.0f, 0.0f, 0.0f, 0.0f);
+		case X:  return FVector4(1.0f, 0.0f, 0.0f, alpha);
+		case Y:  return FVector4(0.0f, 1.0f, 0.0f, alpha);
+		case Z:  return FVector4(0.0f, 0.0f, 1.0f, alpha);
+		default: return FVector4(0.0f, 0.0f, 0.0f, alpha);
 		}
 	}
 
@@ -198,16 +203,12 @@ struct FGizmo {
 		//기즈모타입을 확인후 타입에 맞는 모양을 리턴
 		for (int i = 0; i < 3; ++i)
 		{
-			if(axis[i]==eAxis) { /* highlight */ }
-			renderInfos.Add({ GetAxisPrimitive(), GetAxisMatrix(axis[i]),FObjectID{},GetAxisColor(axis[i])});
+			if (axis[i] == eAxis)
+			{ /* highlight */}
+			renderInfos.Add({ GetAxisPrimitive(), GetAxisMatrix(axis[i]),FObjectID{},GetAxisColor(axis[i]) });
+			
 		}
-		if (eType == ROTATE)
-		{
-			renderInfos.Add({ EPrimitive::EP_Sphere,
-				  FMatrix::Scale(FVector(0.96f * mGizmoScale)) * FMatrix::Translation(mLocation),
-				  FObjectID{},
-				  FVector4(1.0f, 1.0f, 1.0f, 1.0f) });   // 어두운 회색
-		}
+	
 
 		return renderInfos;
 	}
