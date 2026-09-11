77627c84d61c3b85fa520eb6dfa5d7c30b4fd148
chsportfor <chsportfor@gmail.com>
2026-09-11 16:07:56 +0900

    파일 수정 및 GridSpacing 추가

--- Week3/FEditorSetting.h
+++ Week3/FEditorSetting.h
@@ -6,6 +6,7 @@
 struct FEditorSetting
 {
 	float CameraSensitivity = 0.1f;
+	float GridSpacing = 1.0f;
 
 	void Load(const std::string& FilePath = "editor.ini")
 	{
@@ -24,7 +25,12 @@ struct FEditorSetting
 			{
 				File >> CameraSensitivity;
 			}
+			else if (Key == "GridSpacing")
+			{
+				File >> GridSpacing;
+			}
 		}
+		
 	}
 
 	void Save(const std::string& FilePath = "editor.ini") const
@@ -37,5 +43,6 @@ struct FEditorSetting
 		}
 
 		File << "CameraSensitivity " << CameraSensitivity << '\n';
+		File << "GridSpacing " << GridSpacing << '\n';
 	}
 };
