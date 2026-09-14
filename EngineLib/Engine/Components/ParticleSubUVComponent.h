51ebbd9270527ba938132c5f4463e55981edf2b9
Sanghyeon Kim <ksangh9609@gmail.com>
2026-09-14 18:05:45 +0900

    Add ParticleSubUVComopnent

---
+++ EngineLib/Engine/Components/ParticleSubUVComponent.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include "BillboardComponent.h"
+
+#include "Rendering/SubUVMesh.h"
+
+class UParticleSubUVComponent : public UBillboardComponent
+{
+	DECLARE_OBJECT(UParticleSubUVComponent, UBillboardComponent)
+
+public:
+	UParticleSubUVComponent() = default;
+
+	void Initialize(FVector location, FRotator rotation, FVector scale3D,
+		uint32 numRows, uint32 numCols,
+		bool bLooping = true, float playRate = 1.0f, float frameDuration = 1.0f);
+
+	virtual void Update(float deltaTime, TArray<FRenderInfo>* outRenderInfos) override;
+
+private:
+	FSubUVMesh mSubUVMesh;
+
+	/* Particle texture rows and columns */
+	uint32 mNumRows = 1;
+	uint32 mNumCols = 1;
+
+	bool mbLooping = true;
+	float mPlayRate = 1.0f; // Play speed multiplier
+	float mFrameDuration = 1.0f; // Duration of each frame in seconds
+
+	/* Internal State */
+	float mElapsedTime = 0.0f;
+	uint32 mCurrentFrameIndex = 0;
+};
