bb6eca73f2a17430e69f7197544b20ec4205c7ea
Sanghyeon Kim <ksangh9609@gmail.com>
2026-09-14 17:47:30 +0900

    Add SubUVMesh

---
+++ EngineLib/Rendering/SubUVMesh.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "Core/Container/TArray.h"
+
+#include "VertexType.h"
+
+struct FSubUVMesh
+{
+	TArray<FVertexTextured> Vertices;
+	TArray<uint32> Indices;
+
+	FSubUVMesh() = default;
+	FSubUVMesh(float n, float m);
+
+	void UpdateMesh(float n, float m);
+
+private:
+	// Generate the mesh from i-th item of n by m grid
+	void generateMesh(float n, float m, float i);
+};
