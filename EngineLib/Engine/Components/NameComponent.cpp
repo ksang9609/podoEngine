8cf29fc3d08f24c031bf49efa3f24149ee322edb
Sanghyeon Kim <ksangh9609@gmail.com>
2026-09-14 16:27:01 +0900

    Set name component text to show owner's name and uuid

--- EngineLib/Engine/Components/NameComponent.cpp
+++ EngineLib/Engine/Components/NameComponent.cpp
@@ -1,15 +1,18 @@
 ﻿#include "NameComponent.h"
 
+#include <format>
+
+#include "Engine/Actor.h"
+
 IMPLEMENT_CLASS(UNameComponent, UBillboardComponent)
 
 void UNameComponent::Initialize(const FString& nameText, FVector worldPositionOffset, const FFontResource& fontResourceRef)
 {
 	UBillboardComponent::Initialize(worldPositionOffset, FRotator(), FVector(0));
 
-	mNameText = nameText;
 	mFontResourceRef = &fontResourceRef;
 
-	mTextMesh.SetText(mNameText, *mFontResourceRef);
+	mNameText = nameText;
 }
 
 void UNameComponent::updateComponentToWorld(const FMatrix& parentTransform)
@@ -42,8 +45,28 @@ FRenderInfo UNameComponent::makeRenderInfo() const
 
 void UNameComponent::SetNameText(const FString& nameText)
 {
-	mNameText = nameText;
+	assert(mOwner);
+
+	FString text = FString(std::format("Name: {}, UUID: {}", nameText, mOwner->UUID));
+	mNameText = text;
 
 	// TODO: Optimize this by updating in the GetRenderInfos function instead of recreating the FTextMesh every time.
 	mTextMesh.SetText(mNameText, *mFontResourceRef);
+}
+//
+//void UNameComponent::SetNameText(FString&& nameText)
+//{//
+//	// TODO: Optimize this by updating in the GetRenderInfos function instead of recreating the FTextMesh every time.
+//	mTextMesh.SetText(mNameText, *mFontResourceRef);
+//}
+
+bool UNameComponent::AttachTo(USceneComponent& parent)
+{
+	if (!UBillboardComponent::AttachTo(parent))
+	{
+		return false;
+	}
+
+	SetNameText(mOwner->GetName().ToString());
+	return true;
 }
