39b0615801b8afc5c780a254714beb4d927143ec
김현준 <goyoungchun1@gmail.com>
2026-09-22 16:45:30 +0900

    orthogonal에서 raycast 안되는 문제 해결
    orthogonal 카메라의 거리를 200으로 늘렸는데 raycast길이가 100밖에 되지 않아 닿지 않는 문제였음

--- EngineLib/Editor/FEditorViewportClient.cpp
+++ EngineLib/Editor/FEditorViewportClient.cpp
@@ -122,7 +122,7 @@ void FEditorViewportClient::RayCast(const FViewRect& viewrect, const TArray<FRen
 	//		ViewportInfo.Width, ViewportInfo.Height, 0.1f, 100.f, NearPoint, FarPoint);
 	//}
 	DeprojectScreenToWorldForUnified(WindowApplication.Input.CursorX - viewrect.X, WindowApplication.Input.CursorY - viewrect.Y,
-		viewrect.Width, viewrect.Height, 0.1f, 100.f, mCamera.mOrthoDistance, mProjectionRatio, NearPoint, FarPoint);
+		viewrect.Width, viewrect.Height, 0.1f, 250.f, mCamera.mOrthoDistance, mProjectionRatio, NearPoint, FarPoint);
 
 	mRayNear = NearPoint;
 	mRayFar = FarPoint;
